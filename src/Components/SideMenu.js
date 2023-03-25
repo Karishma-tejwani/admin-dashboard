8b0f11909759ff7068f07f9be10bc492e22b26ed
Karishma-tejwani <karishmatejwani9@gmail.com>
2023-03-25 22:23:33 +0500

    Menu

--- src/Components/SideMenu.js
+++ src/Components/SideMenu.js
@@ -1,5 +1,41 @@
+import { Menu } from "antd";
+import {
+  UserOutlined,
+  AppstoreOutlined,
+  ShopOutlined,
+  ShoppingCartOutlined,
+} from "@ant-design/icons";
+
 const SideMenu = () => {
-  return <div className="menu">Menu</div>;
+  return (
+    <div className="menu">
+      <Menu
+        onClick={(item) => {}}
+        items={[
+          {
+            label: "Dashboard",
+            icon: <AppstoreOutlined />,
+            key: "/",
+          },
+          {
+            label: "Inventory",
+            icon: <ShopOutlined />,
+            key: "/inventory",
+          },
+          {
+            label: "Orders",
+            icon: <ShoppingCartOutlined />,
+            key: "/orders",
+          },
+          {
+            label: "Customers",
+            icon: <UserOutlined />,
+            key: "/customers",
+          },
+        ]}
+      ></Menu>
+    </div>
+  );
 };
 
 export default SideMenu;
