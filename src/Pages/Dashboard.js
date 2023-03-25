60cac844b798767a736c2ec2da9457beaf096409
Karishma-tejwani <karishmatejwani9@gmail.com>
2023-03-25 23:11:30 +0500

    Dashboard

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -13,22 +13,62 @@ const Dashboard = () => {
       <Typography.Title level={3}>Dashboard</Typography.Title>
       <Space direction="horizontal">
         <DashboardCard
-          icon={<ShoppingCartOutlined />}
+          icon={
+            <ShoppingCartOutlined
+              style={{
+                color: "green",
+                backgroundColor: "rgba(0,255,0,0.25)",
+                borderRadius: 20,
+                fontSize: 24,
+                padding: 8,
+              }}
+            />
+          }
           title={"Orders"}
           value={1024}
         />
         <DashboardCard
-          icon={<ShoppingOutlined />}
+          icon={
+            <ShoppingOutlined
+              style={{
+                color: "blue",
+                backgroundColor: "rgba(0,0,255,0.25)",
+                borderRadius: 20,
+                fontSize: 24,
+                padding: 8,
+              }}
+            />
+          }
           title={"Inventories"}
           value={2563}
         />
         <DashboardCard
-          icon={<UserOutlined />}
+          icon={
+            <UserOutlined
+              style={{
+                color: "purple",
+                backgroundColor: "rgba(0,255,255,0.25)",
+                borderRadius: 20,
+                fontSize: 24,
+                padding: 8,
+              }}
+            />
+          }
           title={"Customers"}
           value={500}
         />
         <DashboardCard
-          icon={<DollarCircleOutlined />}
+          icon={
+            <DollarCircleOutlined
+              style={{
+                color: "red",
+                backgroundColor: "rgba(255,0,0,0.25)",
+                borderRadius: 20,
+                fontSize: 24,
+                padding: 8,
+              }}
+            />
+          }
           title={"Revenue"}
           value={150000}
         />
