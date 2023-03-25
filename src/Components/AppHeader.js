6440f18a39d6fbc57c5485fac76eea3efa18a2fa
Karishma-tejwani <karishmatejwani9@gmail.com>
2023-03-25 15:46:10 +0500

    manifest.json

--- src/Components/AppHeader.js
+++ src/Components/AppHeader.js
@@ -1,5 +1,21 @@
+import { Descriptions } from "antd";
+
 const AppHeader = () => {
-  return <div className="header">Header</div>;
+  return (
+    <div className="header">
+      <Descriptions
+        title="Dashboard"
+        column={{
+          xxl: 4,
+          xl: 3,
+          lg: 3,
+          md: 3,
+          sm: 2,
+          xs: 1,
+        }}
+      ></Descriptions>
+    </div>
+  );
 };
 
 export default AppHeader;
