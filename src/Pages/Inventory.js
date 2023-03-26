3228a95b22fb3def27cf35113d2beb5ee49a4bd5
Karishma-tejwani <karishmatejwani9@gmail.com>
2023-03-27 00:23:25 +0500

    Inventory

--- src/Pages/Inventory.js
+++ src/Pages/Inventory.js
@@ -1,12 +1,69 @@
-import React from "react";
-import { Typography } from "antd";
+import React, { useEffect, useState } from "react";
+import { Avatar, Rate, Space, Table, Typography } from "antd";
 import getInventory from "./API/getInventory";
 
 const Inventory = () => {
+  const [load, setLoad] = useState(false);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    setLoad(true);
+    getInventory().then((res) => {
+      setData(res.products);
+      setLoad(false);
+    });
+  }, []);
+
   return (
-    <div>
-      <Typography.Title level={3}>Inventory</Typography.Title>
-    </div>
+    <Space size={20} direction="vertical">
+      <Typography.Title level={3} style={{ textAlign: "center" }}>
+        Inventory
+      </Typography.Title>
+      <Table
+        loading={load}
+        columns={[
+          {
+            title: "Thumbnail",
+            dataIndex: "thumbnail",
+            render: (link) => {
+              return <Avatar src={link} />;
+            },
+          },
+          {
+            title: "Title",
+            dataIndex: "title",
+          },
+          {
+            title: "Price",
+            dataIndex: "price",
+            render: (val) => <span>${val}</span>,
+          },
+          {
+            title: "Rating",
+            dataIndex: "rating",
+            render: (rate) => {
+              return <Rate value={rate} allowHalf disabled />;
+            },
+          },
+          {
+            title: "Stock",
+            dataIndex: "stock",
+          },
+          {
+            title: "Brand",
+            dataIndex: "brand",
+          },
+          {
+            title: "Category",
+            dataIndex: "category",
+          },
+        ]}
+        dataSource={data}
+        pagination={{
+          pageSize: 6,
+        }}
+      ></Table>
+    </Space>
   );
 };
 
